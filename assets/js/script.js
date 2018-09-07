4982bd96af11b63b93660a30b24f4b13862a9369
Kamen <kamen.boev@gmail.com>
2018-09-07 14:11:49 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
-// It's reffered in index.html but empty so far
 // TODO: move the code from <script> tag here
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", "true");
+
+document.getElementById('tweet-btn').addEventListener('click', getTweets);
+
+function getTweets() {
+	console.log('clicked');
+	if(xhr.status == 200) {
+		console.log(this);
+	}
+	
+}
